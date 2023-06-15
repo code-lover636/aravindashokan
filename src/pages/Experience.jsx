51a1a4e377c2ea2e5275c71fb90067a8f59bc13d
Aravind Ashokan <aravindashokan636@gmail.com>
2023-06-15 20:01:11 +0530

    Update Changes

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -19,7 +19,7 @@ const Expdetails = props =>{
 }
 
 const Experience = () => {
-  const hac = useRef(0), int = useRef(1), edu = useRef(2);
+  const hac = useRef(0), int = useRef(1), pos = useRef(2);
   const internExp = [
     {
         "desig": "UI/UX Designer",
@@ -36,9 +36,15 @@ const Experience = () => {
   ];
   const hackExp = [
     {
+        "desig": "SlashKey",
+        "date": "May 2023",
+        "company": "IEEE CUSAT | INTEL",
+        "content": "Worked on a music recommeder website based on machine learning."
+    },
+    {
         "desig": "Devjam",
         "date": "May 2023",
-        "company": "Script Foundation | Devfolio",
+        "company": "Devfolio",
         "content": "Worked on a web based project(ChatSecure) which uses machine learning and NLP to classify spam messages."
     },
     {
@@ -60,24 +66,36 @@ const Experience = () => {
         "content": ""
     }
   ];
-  const eduExp = [
+  const posExp = [
     {
-        "desig": "Computer Science Engineering",
-        "date": "2022 - 2026 [present]",
-        "company": "Govt. Model Engineering College",
+        "desig": "Tech Team Core Member",
+        "date": "May 2023 - [present]",
+        "company": "FOSS MEC",
         "content": ""
     },
     {
-        "desig": "12 standard",
-        "date": "2021-2022",
-        "company": "CBSE: Parama Bhattara Kendriya Vidyalayam",
-        "content": "Percentage: 96.4%"
+        "desig": "Design Team Core Member",
+        "date": "May 2023 - [present]",
+        "company": "FOSS MEC",
+        "content": ""
     },
     {
-        "desig": "10 standard",
-        "date": "2019-2020",
-        "company": "CBSE: Parama Bhattara Kendriya Vidyalayam",
-        "content": "Percentage: 95.6%"
+        "desig": "UI/UX Team Core Member",
+        "date": "May 2023 - [present]",
+        "company": "FOSS MEC",
+        "content": ""
+    },
+    {
+        "desig": "Tech Trainee",
+        "date": "May 2023 - [present]",
+        "company": "TinkerHub MEC",
+        "content": ""
+    },
+    {
+        "desig": "Design Team Member",
+        "date": "Apr 2023 - [present]",
+        "company": "TBT MEC",
+        "content": ""
     },
     {
         "desig": "",
@@ -94,29 +112,29 @@ const Experience = () => {
                 setDisplay(internExp); 
                 hac.current.classList.remove("active");
                 int.current.classList.add("active");
-                edu.current.classList.remove("active");
+                pos.current.classList.remove("active");
                 }}>
                 <svg className="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M20 6H16V4C16 2.89 15.11 2 14 2H10C8.89 2 8 2.89 8 4V6H4C2.89 6 2.01 6.89 2.01 8L2 19C2 20.11 2.89 21 4 21H20C21.11 21 22 20.11 22 19V8C22 6.89 21.11 6 20 6ZM14 6H10V4H14V6Z" fill="#F8F8F8"/>
                 </svg>
                 Internships
-                </h2>
+            </h2>
             <h2 ref={hac} onClick={() => {
                 setDisplay(hackExp); 
                 hac.current.classList.add("active");
                 int.current.classList.remove("active");
-                edu.current.classList.remove("active");
+                pos.current.classList.remove("active");
                 }}>
                 <svg className="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2 21C1.71667 21 1.479 20.904 1.287 20.712C1.095 20.52 0.999337 20.2827 1 20C1 19.7167 1.096 19.479 1.288 19.287C1.48 19.095 1.71734 18.9993 2 19H22C22.2833 19 22.521 19.096 22.713 19.288C22.905 19.48 23.0007 19.7173 23 20C23 20.2833 22.904 20.521 22.712 20.713C22.52 20.905 22.2827 21.0007 22 21H2ZM4 18C3.45 18 2.979 17.804 2.587 17.412C2.195 17.02 1.99934 16.5493 2 16V5C2 4.45 2.196 3.979 2.588 3.587C2.98 3.195 3.45067 2.99934 4 3H20C20.55 3 21.021 3.196 21.413 3.588C21.805 3.98 22.0007 4.45067 22 5V16C22 16.55 21.804 17.021 21.412 17.413C21.02 17.805 20.5493 18.0007 20 18H4Z" fill="#F8F8F8"/>
                 </svg>
                 Hackathons
                 </h2>
-            <h2 ref={edu} onClick={() => {
-                setDisplay(eduExp); 
+            <h2 ref={pos} onClick={() => {
+                setDisplay(posExp); 
                 hac.current.classList.remove("active");
                 int.current.classList.remove("active");
-                edu.current.classList.add("active");
+                pos.current.classList.add("active");
                 }}>
                 <svg className="icon" width="640" height="512" viewBox="0 0 640 512" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g clipPath="url(#clip0_80_6)">
@@ -128,13 +146,13 @@ const Experience = () => {
                     </clipPath>
                     </defs>
                 </svg>
-                Education
+                Positions
             </h2>
 
         </header>
         <div className="container">
             <div className="card">
-                { display.map( exp => <Expdetails key={exp["date"]} desig={exp["desig"]} date={exp["date"]} company={exp["company"]} content={exp["content"]}/> ) }
+                { display.map( exp => <Expdetails desig={exp["desig"]} date={exp["date"]} company={exp["company"]} content={exp["content"]}/> ) }
             </div>
             <div className="line"></div>
         </div>
